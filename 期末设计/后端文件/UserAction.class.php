d06b7b047705918393ad7b2dadd962399531ff5c
Amaninthedark <3524300975@qq.com>
2018-08-17 20:31:32 +0800

    1

---
+++ 期末设计/后端文件/UserAction.class.php
@@ -0,0 +1,30 @@
+﻿<?php
+// 本类由系统自动生成，仅供测试用途
+class UserAction extends Action {
+      public function qimo(){
+
+    $nation = D("xufeng");
+    $a = $nation->select();
+    echo json_encode($a);
+  }
+   public function tianjiaq(){
+     $data['xuehao']  = $_GET['xuehao'];
+     $data['name']  =$_GET['name'];
+    $re = M('xufeng')->add($data);
+    
+    echo $re;
+  }
+   public function delete(){
+     $id  = $_GET['id'];
+    $res =M('xufeng') ->where("id=$id") ->delete();
+    echo $res;
+  }
+    public function upset(){
+     $User = M("xufeng"); // 实例化User对象
+    // 要修改的数据对象属性赋值
+      $id=$_GET['id'];
+    $User->xuehao = $_GET['xuehao'];
+    $User->name =  $_GET['name'];
+   $User->where("id=$id")->save(); // 根据条件更新记录
+  }
+}
